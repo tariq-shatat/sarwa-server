38747b23d9434b1f1368da7fc61f1811ca961661
Tariq Shatat <tariq.s.shatat@gmail.com>
2022-11-27 19:33:42 +0200

    initial commit

---
+++ src/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Success!")
+	log.Info().Msg("Endpoint Hit: home")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", home)
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Cannot start service.")
+	}
+}
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Info().Msg("Starting up Go server.")
+	handleRequests()
+}
